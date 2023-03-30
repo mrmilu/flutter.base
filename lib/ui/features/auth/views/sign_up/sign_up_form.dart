ccf4d78d58267b7fe6d6cd3c072ed0b0c8ee9dd5
Matias de Andrea <mdeandrea@mrmilu.com>
2023-03-30 11:30:04 +0200

    fix: update misstype

--- lib/ui/features/auth/views/sign_up/sign_up_form.dart
+++ lib/ui/features/auth/views/sign_up/sign_up_form.dart
@@ -25,7 +25,7 @@ class SignUpForm extends ConsumerWidget {
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
               ReactiveInput(
-                key: const Key('sing_up_name'),
+                key: const Key('sign_up_name'),
                 formControl: formModel.nameControl,
                 placeholder: LocaleKeys.signUp_form_name_label.tr(),
                 textCapitalization: TextCapitalization.words,
@@ -33,7 +33,7 @@ class SignUpForm extends ConsumerWidget {
               ),
               BoxSpacer.v16(),
               ReactiveInput(
-                key: const Key('sing_up_email'),
+                key: const Key('sign_up_email'),
                 formControl: formModel.emailControl,
                 placeholder: LocaleKeys.signUp_form_email_label.tr(),
                 keyboardType: TextInputType.emailAddress,
@@ -41,7 +41,7 @@ class SignUpForm extends ConsumerWidget {
               ),
               BoxSpacer.v16(),
               PasswordReactiveInput(
-                key: const Key('sing_up_pass'),
+                key: const Key('sign_up_pass'),
                 formControl: formModel.passwordControl,
                 placeholder: LocaleKeys.signUp_form_password_label.tr(),
                 validationMessages: {
@@ -53,7 +53,7 @@ class SignUpForm extends ConsumerWidget {
               ReactiveSignUpModelFormConsumer(
                 builder: (context, consumerModel, _) {
                   return ButtonPrimary(
-                    key: const Key('sing_up_button'),
+                    key: const Key('sign_up_button'),
                     text: LocaleKeys.signUp_form_submit.tr(),
                     onPressed: consumerModel.form.valid
                         ? () {
