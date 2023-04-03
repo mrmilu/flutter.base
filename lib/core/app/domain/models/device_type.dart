91b5b92e2de97fe2c3e314d33da43099c298033a
Matias de Andrea <mdeandrea@mrmilu.com>
2023-04-03 09:22:49 +0200

    feat: create device type on domain

---
+++ lib/core/app/domain/models/device_type.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_base/core/user/domain/enums/user_device_type.dart';
+import 'package:flutter_mrmilu/flutter_mrmilu.dart';
+
+class DeviceType {
+  static UserDeviceType get deviceType => PlatformUtils.isIOS
+      ? UserDeviceType.ios
+      : PlatformUtils.isAndroid
+          ? UserDeviceType.android
+          : UserDeviceType.unknown;
+}
