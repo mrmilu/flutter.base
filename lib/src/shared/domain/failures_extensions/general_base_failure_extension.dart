b7b598a81b2427c132469244a5563c5bb381f965
JUNGLE <anullos988@gmail.com>
2025-08-13 09:28:45 +0200

    feat: clean

--- lib/src/shared/domain/failures_extensions/general_base_failure_extension.dart
+++ lib/src/shared/domain/failures_extensions/general_base_failure_extension.dart
@@ -2,7 +2,6 @@ import 'package:flutter/cupertino.dart';
 
 import '../../presentation/l10n/generated/l10n.dart';
 import '../failures/general_base_failure.dart';
-import '../failures/get_user_failure.dart';
 
 extension GeneralBaseFailureExtension on GeneralBaseFailure {
   String toTranslation(BuildContext context) {
@@ -14,16 +13,6 @@ extension GeneralBaseFailureExtension on GeneralBaseFailure {
       invalidResponseFormat: (_, _) =>
           'Formato de respuesta inválido. Contacta soporte.',
       unexpectedError: (_, _) => 'Error inesperado. Intenta más tarde.',
-    );
-  }
-}
-
-extension GetUserFailureExtension on GetUserFailure {
-  String toTranslation(BuildContext context) {
-    return when(
-      userNotFound: (_, _) => S.of(context).cancelledByUser,
-      userInvalid: (_, _) => S.of(context).internalError,
-      general: (appError) => appError.toTranslation(context),
     );
   }
 }
---
+++ lib/src/shared/domain/failures_extensions/get_user_failure_extension.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+import '../../presentation/l10n/generated/l10n.dart';
+import '../failures/get_user_failure.dart';
+import 'general_base_failure_extension.dart';
+
+extension GetUserFailureExtension on GetUserFailure {
+  String toTranslation(BuildContext context) {
+    return when(
+      userNotFound: (_, _) => S.of(context).cancelledByUser,
+      userInvalid: (_, _) => S.of(context).internalError,
+      general: (appError) => appError.toTranslation(context),
+    );
+  }
+}
